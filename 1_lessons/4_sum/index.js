4bb7da72086bf65077f8dd4fbccc31140ab9427a
Dmitriy N <dpolarov@gmail.com>
2020-05-14 17:17:59 +0300

    4) arithmetic mean

---
+++ 1_lessons/4_sum/index.js
@@ -0,0 +1,13 @@
+﻿var sum = 0;
+var count = 0;
+while (true) {
+  var value = +prompt("input numer", '');
+  if (!value) break; // (*)
+  count++;
+  sum += value;
+  mean = sum / count;
+  console.log('arithmetic mean: ' + mean + ' Count number: ' + count);
+}
+
+alert( 'arithmetic mean: ' + mean + ' Count number: ' + count);
+
