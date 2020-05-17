856a81edaefd06ef1b5af3a8fc33c086e90b913e
Dmitriy N <dpolarov@gmail.com>
2020-05-17 13:31:23 +0300

    1) sum of negative numbers

---
+++ 2_lessons/1_sum-object/index.js
@@ -0,0 +1,22 @@
+﻿let num = [91, 93, 45, -67, -96, -40, 34, -96, 42, -58];
+let result_object={};
+
+let function_sum = function(arrs){
+  let count = 0;
+  let sum = 0;
+  for (let arr of arrs) {
+   //    alert( arr ); 
+    if (arr < 0 ){
+      console.log(arr);
+      count++;
+      sum = sum + arr;
+    } 
+
+  } 
+  let result_object={count,sum};
+  return result_object;
+}
+let res_obj = function_sum(num);
+
+console.log (res_obj)
+
