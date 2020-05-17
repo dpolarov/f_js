e42ed6b01d38d949a0440f49b97590c78faf192b
Dmitriy N <dpolarov@gmail.com>
2020-05-17 14:38:20 +0300

    2) parse and sum string

---
+++ 2_lessons/2_parse_string/index.js
@@ -0,0 +1,22 @@
+﻿//let str = prompt("input string", 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed');
+let str = 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed';
+
+let parse_word = function(input_str){
+  let arrs_str = input_str.split(/[\s+]/);
+
+  let result_obj=[];
+   for ( word of arrs_str) {
+
+      let res = {};
+      res.word = word;
+      res.sum = word.split('').reduce((a, b) => a + b.charCodeAt(0),0);
+//      console.log(res.word, res.sum);
+      result_obj.push(res);
+  } 
+  return result_obj;
+}
+
+
+
+console.table (parse_word(str));
+
