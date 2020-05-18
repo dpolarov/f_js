738823462edb8df5ec33f723eb31fc5b6162919a
Dmitriy N <dpolarov@gmail.com>
2020-05-18 14:17:40 +0300

    array metod and function

--- 2_lessons/1_sum-object/index.js
+++ 2_lessons/1_sum-object/index.js
@@ -1,21 +1,16 @@
 ﻿let num = [91, 93, 45, -67, -96, -40, 34, -96, 42, -58];
-let result_object={};
 
-let function_sum = function(arrs){
-  let count = 0;
-  let sum = 0;
-  for (let arr of arrs) {
-   //    alert( arr ); 
-    if (arr < 0 ){
-      console.log(arr);
-      count++;
-      sum = sum + arr;
-    } 
+function function_sum(arrs){
+   
+  // незнаю как лучше  find /map ? 
+  let new_arr=arrs.filter(id => id  < 0);
+  let sum = new_arr.reduce((sum, current) => sum + current, 0);
+  let count =  new_arr.length;
 
-  } 
   let result_object={count,sum};
   return result_object;
 }
+
 let res_obj = function_sum(num);
 
 console.log (res_obj)
